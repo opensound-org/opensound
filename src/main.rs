6ce2d89334a0e385f990416c546e16e5c694ad9e
29 <791603901@qq.com>
2024-04-06 22:34:08 +0800

    Add initial command-line entry
    添加初始命令行入口

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use clap::{crate_version, Parser};
 use opensound::ostp;
 
+/// OpenSound PoC Binary
+#[derive(Parser, Debug)]
+#[command(version = concat!("v", crate_version!()), author, long_about = None)]
+enum Commands {
+    /// Boot the API Server
+    Boot,
+    #[command(hide = true)]
+    Timer,
+}
+
 fn main() {
+    println!("{:?}", Commands::parse());
+
     ostp::install_default();
     ostp::emit::debug("Hello, world!", "main", None, true);
 
