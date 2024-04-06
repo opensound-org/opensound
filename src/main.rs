e80f2c7ed4a9812d39e4c190f5b6d04f41ca2950
29 <791603901@qq.com>
2024-04-06 22:39:46 +0800

    F**k clap

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use opensound::ostp;
 /// OpenSound PoC Binary
 #[derive(Parser, Debug)]
 #[command(version = concat!("v", crate_version!()), author, long_about = None)]
+// TMD clap不支持中文，因此命令行帮助文档暂时就没办法i18n或者l10n了……
 enum Commands {
     /// Boot the API Server
     Boot,
