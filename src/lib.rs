73094af8e58b15425575750ac3408b94c6bf14b0
29 <791603901@qq.com>
2024-04-07 15:49:08 +0800

    Add some doc comments
    添加一些文档注释

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,23 @@
+/// Some common components
+/// 
+/// 一些通用组件
 pub mod common {
     pub mod ostp;
 }
+/// The `Gadget` concept
+/// 
+/// `Gadget` 概念
+/// 
+/// Detailed documentation is todo
+/// 
+/// 详细文档todo
 pub mod gadgets {
     pub mod timer;
     pub mod uuid;
 }
+/// The OpenSound Kernel
+/// 
+/// OpenSound内核
 pub mod kernel;
 
 /// Default boot procedure entry
