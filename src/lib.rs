c7e20cadbf250953bf98d43d598a6802cda34c58
29 <791603901@qq.com>
2024-04-08 15:40:50 +0800

    Cleanup unused function
    清理未使用的函数

--- src/lib.rs
+++ src/lib.rs
@@ -48,18 +48,3 @@ pub async fn boot() -> CommonRes {
     ostp::install_default();
     kernel::main().await
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
