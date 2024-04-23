74dfdb2b31437a73b7eff2b183b184f6dde3df0b
29 <791603901@qq.com>
2024-04-23 16:39:26 +0800

    Add websocket server template
    添加WebSocket服务器模板

--- src/microkernel.rs
+++ src/microkernel.rs
@@ -11,5 +11,33 @@ pub async fn main() -> CommonRes {
         "main",
         None,
     );
-    Ok(())
+
+    use futures::{SinkExt, StreamExt};
+    use tokio::net::TcpListener;
+    use tokio_tungstenite::{accept_hdr_async, tungstenite::handshake::server::Request};
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            if let Ok(mut websocket) = accept_hdr_async(socket, |req: &Request, res| {
+                if req.uri().path() == "/test" {
+                    Ok(res)
+                } else {
+                    Err(Default::default())
+                }
+            })
+            .await
+            {
+                while let Some(Ok(msg)) = websocket.next().await {
+                    // We do not want to send back ping/pong messages.
+                    if msg.is_binary() || msg.is_text() {
+                        websocket.send(msg).await.ok();
+                    }
+                }
+            }
+        });
+    }
 }
