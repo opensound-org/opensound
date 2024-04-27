fe7ef0112c33c22acf1cc9dec90b86e0dbfe3778
29 <791603901@qq.com>
2024-04-27 15:07:25 +0800

    Update `microkernel`
    更新微内核

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,11 @@ pub mod common {
 
     /// Result<(), anyhow::Error>
     pub type CommonRes = Result<(), anyhow::Error>;
+
+    use std::{future::Future, pin::Pin};
+
+    /// Pin<Box<dyn Future<Output = CommonRes> + Send>>
+    pub type CommonFut = Pin<Box<dyn Future<Output = CommonRes> + Send>>;
 }
 /// The `Gadget` concept
 ///
@@ -39,6 +44,25 @@ mod microkernel {
     mod init;
     pub use init::*;
 
+    mod backends {
+        #[cfg(feature = "actix-web")]
+        pub mod actix_web;
+        #[cfg(feature = "axum")]
+        pub mod axum;
+        #[cfg(feature = "ntex")]
+        pub mod ntex;
+        #[cfg(feature = "poem")]
+        pub mod poem;
+        #[cfg(feature = "rocket")]
+        pub mod rocket;
+        #[cfg(feature = "salvo")]
+        pub mod salvo;
+        #[cfg(feature = "viz")]
+        pub mod viz;
+        #[cfg(feature = "warp")]
+        pub mod warp;
+    }
+
     #[allow(dead_code)]
     mod reqres;
 }
@@ -49,11 +73,12 @@ mod microkernel {
 pub const VERSION: &'static str = concat!("v", clap::crate_version!());
 
 use common::{ostp, CommonRes};
+use microkernel::MicroKernel;
 
 /// Default boot procedure entry
 ///
 /// 默认启动过程入口
 pub async fn boot_default() -> CommonRes {
     ostp::install_default();
-    microkernel::launch().await
+    MicroKernel::launch(None).await?.join().await
 }
--- src/microkernel/init.rs
+++ src/microkernel/init.rs
@@ -1,4 +1,7 @@
-use crate::common::{ostp, CommonRes};
+use super::backends;
+use crate::common::{ostp, CommonFut, CommonRes};
+use std::net::SocketAddr;
+use tokio::task::JoinHandle;
 
 async fn _ws() -> CommonRes {
     use futures::{SinkExt, StreamExt};
@@ -31,13 +34,46 @@ async fn _ws() -> CommonRes {
     }
 }
 
-pub async fn launch() -> CommonRes {
-    ostp::emit::debug(
-        "Hello, world!",
-        Some("你好，世界！"),
-        "microkernel",
-        "main",
-        None,
-    );
-    Ok(())
+pub struct MicroKernel {
+    _http_port: u16,
+    http_server: JoinHandle<CommonRes>,
+}
+
+impl MicroKernel {
+    pub async fn launch(port: Option<u16>) -> Result<Self, anyhow::Error> {
+        let (_http_port, http_server) = Self::launch_backend(backends::axum::ignite(port).await)?;
+        Ok(Self {
+            _http_port,
+            http_server,
+        })
+    }
+
+    pub async fn join(self) -> CommonRes {
+        self.http_server.await.unwrap()
+    }
+
+    fn launch_backend(
+        (backend, res): (&'static str, Result<(SocketAddr, CommonFut), anyhow::Error>),
+    ) -> Result<(u16, JoinHandle<CommonRes>), anyhow::Error> {
+        match res {
+            Ok((addr, future)) => {
+                let message_en = format!("{} launched at: {}", backend, addr);
+                let message_zh = Some(format!("{}启动于：{}", backend, addr));
+                let message_zh = message_zh.as_deref();
+                ostp::emit::info(&message_en, message_zh, "sys", "launch", None);
+
+                let http_port = addr.port();
+                let http_server = tokio::spawn(future);
+
+                Ok((http_port, http_server))
+            }
+            Err(err) => {
+                let message_en = format!("{} launch failed! Reason: {}", backend, err);
+                let message_zh = Some(format!("{}启动失败！原因：{}", backend, err));
+                let message_zh = message_zh.as_deref();
+                ostp::emit::error(&message_en, message_zh, "sys", "launch", None);
+                Err(err)
+            }
+        }
+    }
 }
