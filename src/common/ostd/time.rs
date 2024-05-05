a580e93bd0251f871fb58371afbdd44305f2d139
29 <791603901@qq.com>
2024-05-05 19:34:15 +0800

    Add `ostd::time::sleep_infinite()`

--- src/common/ostd/time.rs
+++ src/common/ostd/time.rs
@@ -13,3 +13,17 @@ use std::time::Duration;
 pub async fn sleep_minimal() {
     tokio::time::sleep(Duration::from_millis(1)).await
 }
+
+/// Sleep infinite time
+///
+/// 睡眠无穷大的时间
+///
+/// This function is useful for those who want an asynchronous,
+/// infinite loop that does not occupy the CPU.
+///
+/// 这个函数对于想要一个异步的，不占用CPU的死循环很有用。
+pub async fn sleep_infinite() {
+    loop {
+        sleep_minimal().await
+    }
+}
