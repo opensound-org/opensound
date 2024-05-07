3c07adac8b5f47f7376e500cc094b70282887fdc
29 <791603901@qq.com>
2024-05-07 22:38:02 +0800

    Implementing the `Graceful Shutdown` interface for the `Axum` backend
    为“Axum”后端实现“优雅停机”接口

--- src/microkernel/backends/axum.rs
+++ src/microkernel/backends/axum.rs
@@ -1,10 +1,10 @@
 use super::super::reqres::SysCtrl;
 use crate::common::CommonFut;
-use axum::{response::Json, routing::get, Router};
+use axum::{extract::State, response::Json, routing::get, Router};
 use futures::FutureExt;
 use serde_json::Value;
 use std::{
-    future::IntoFuture,
+    future::{Future, IntoFuture},
     net::{Ipv4Addr, SocketAddr},
 };
 use tokio::net::TcpListener;
@@ -23,20 +23,44 @@ async fn version() -> Json<Value> {
     Json(SysCtrl::version())
 }
 
-async fn ignite_internal(port: Option<u16>) -> Result<(SocketAddr, CommonFut), anyhow::Error> {
-    let router = Router::new()
+async fn shutdown(State(state): State<Option<SysCtrl>>) -> &'static str {
+    state.unwrap().trigger_shutdown()
+}
+
+async fn reboot(State(state): State<Option<SysCtrl>>) -> &'static str {
+    state.unwrap().trigger_reboot()
+}
+
+async fn ignite_internal(
+    port: Option<u16>,
+    ctrl: Option<SysCtrl>,
+    graceful_shutdown: impl Future<Output = ()> + Send + 'static,
+) -> Result<(SocketAddr, CommonFut), anyhow::Error> {
+    let mut router = Router::new()
         .route("/", get(index))
         .route("/api/v1/sys/hello", get(hello))
         .route("/api/v1/sys/version", get(version));
+
+    if ctrl.is_some() {
+        router = router
+            .route("/api/v1/sys/shutdown", get(shutdown))
+            .route("/api/v1/sys/reboot", get(reboot));
+    }
+
+    let router = router.with_state(ctrl);
     let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, port.unwrap_or(0))).await?;
     let addr = listener.local_addr().unwrap();
-    let fut = axum::serve(listener, router).into_future();
+    let fut = axum::serve(listener, router)
+        .with_graceful_shutdown(graceful_shutdown)
+        .into_future();
 
     Ok((addr, async move { Ok(fut.await?) }.boxed()))
 }
 
 pub async fn ignite(
     port: Option<u16>,
+    ctrl: Option<SysCtrl>,
+    graceful_shutdown: impl Future<Output = ()> + Send + 'static,
 ) -> (&'static str, Result<(SocketAddr, CommonFut), anyhow::Error>) {
-    (NAME, ignite_internal(port).await)
+    (NAME, ignite_internal(port, ctrl, graceful_shutdown).await)
 }
