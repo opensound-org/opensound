3677f7e1b5c5e26a00376e74ccf2bcc77d2e4085
29 <791603901@qq.com>
2024-05-08 23:32:48 +0800

    Implementing the `Graceful Shutdown` interface for the `Warp` backend
    为“Warp”后端实现“优雅停机”接口

--- src/microkernel/backends/warp.rs
+++ src/microkernel/backends/warp.rs
@@ -1,24 +1,56 @@
 use super::super::reqres::SysCtrl;
 use crate::common::CommonFut;
 use futures::FutureExt;
-use std::net::{Ipv4Addr, SocketAddr};
-use warp::{reply::json, Filter};
+use std::{
+    future::Future,
+    net::{Ipv4Addr, SocketAddr},
+};
+use warp::{http::StatusCode, reply::json, Filter};
 
 const NAME: &'static str = "Warp";
 
-async fn ignite_internal(port: Option<u16>) -> Result<(SocketAddr, CommonFut), anyhow::Error> {
-    let index = warp::path!().map(|| SysCtrl::http_index(NAME));
-    let hello = warp::path!("api" / "v1" / "sys" / "hello").map(|| SysCtrl::hello());
-    let version = warp::path!("api" / "v1" / "sys" / "version").map(|| json(&SysCtrl::version()));
-    let routes = warp::get().and(index.or(hello).or(version));
-    let (addr, fut) =
-        warp::serve(routes).try_bind_ephemeral((Ipv4Addr::UNSPECIFIED, port.unwrap_or(0)))?;
+async fn ignite_internal(
+    port: Option<u16>,
+    ctrl: Option<SysCtrl>,
+    graceful_shutdown: impl Future<Output = ()> + Send + 'static,
+) -> Result<(SocketAddr, CommonFut), anyhow::Error> {
+    let (ctrl_s, ctrl_r) = (ctrl.clone(), ctrl);
+    let index = warp::path!().then(|| async move { SysCtrl::http_index(NAME) });
+    let hello =
+        warp::path!("api" / "v1" / "sys" / "hello").then(|| async move { SysCtrl::hello() });
+    let version = warp::path!("api" / "v1" / "sys" / "version")
+        .then(|| async move { json(&SysCtrl::version()) });
+    let shutdown = warp::path!("api" / "v1" / "sys" / "shutdown").then(move || {
+        let ctrl = ctrl_s.clone();
+        async move {
+            match ctrl {
+                Some(ctrl) => Ok(ctrl.trigger_shutdown()),
+                None => Err(StatusCode::NOT_FOUND),
+            }
+        }
+    });
+    let reboot = warp::path!("api" / "v1" / "sys" / "reboot").then(move || {
+        let ctrl = ctrl_r.clone();
+        async move {
+            match ctrl.clone() {
+                Some(ctrl) => Ok(ctrl.trigger_reboot()),
+                None => Err(StatusCode::NOT_FOUND),
+            }
+        }
+    });
+    let routes = warp::get().and(index.or(hello).or(version).or(shutdown).or(reboot));
+    let (addr, fut) = warp::serve(routes).try_bind_with_graceful_shutdown(
+        (Ipv4Addr::UNSPECIFIED, port.unwrap_or(0)),
+        graceful_shutdown,
+    )?;
 
     Ok((addr, async move { Ok(fut.await) }.boxed()))
 }
 
 pub async fn ignite(
     port: Option<u16>,
+    ctrl: Option<SysCtrl>,
+    graceful_shutdown: impl Future<Output = ()> + Send + 'static,
 ) -> (&'static str, Result<(SocketAddr, CommonFut), anyhow::Error>) {
-    (NAME, ignite_internal(port).await)
+    (NAME, ignite_internal(port, ctrl, graceful_shutdown).await)
 }
